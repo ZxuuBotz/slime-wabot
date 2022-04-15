04da8914f703632524340c2b47dc56b02942aa49
DanzGantevk75 <102861930+DanzGantevk75@users.noreply.github.com>
2022-04-15 08:12:04 +0700

    Update gcbot.js

--- plugins/gcbot.js
+++ plugins/gcbot.js
@@ -2,7 +2,7 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 let pp = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=Group'
 let botol = global.botwm
 let str = `
-✧─────[ *Group Official* ]─────✧
+✧─────[ *Group Official DanzBot* ]─────✧
 🍀 Group 1 :
 https://chat.whatsapp.com/KuW6Ojk8b3NF8GIpt9v4EL
 🍀 Group 2 :
