4f7e631d5905e52fd4c8444a473ef966e52311b7
DanzGantevk75 <102861930+DanzGantevk75@users.noreply.github.com>
2022-04-15 08:34:25 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -5,29 +5,29 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     contacts: [{
 
 
-"displayName": "CREATOR•SLIME",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:CREATOR•SLIME\nitem1.TEL;waid=6281354079196:6281354079196\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:rimuru418@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Adri158\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Tokyo 🇯🇵\nitem5.X-ABLabel:───────[ SLIMEBOT ]───────\nEND:VCARD"
+"displayName": "CREATOR DANZZ",
+      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:CREATOR DANZZ\nitem1.TEL;waid=6288270863279:6288270863279\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:witmawati4@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Adri158\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Tokyo 🇯🇵\nitem5.X-ABLabel:───────[ DanzBot ]───────\nEND:VCARD"
  }, {
    "displayName": "BABI",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:BABI\nitem1.TEL;waid=6281354079196:6281354079196\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:rimuru418@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Adri158\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Tokyo 🇯🇵\nitem5.X-ABLabel:───────[ SLIMEBOT ]───────\nEND:VCARD"
+      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:BABI\nitem1.TEL;waid=6288270863279:6288270863379\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:witmawati4@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Adri158\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Tokyo 🇯🇵\nitem5.X-ABLabel:───────[ DanzBot ]───────\nEND:VCARD"
   }, {
    "displayName": "ANAK ANJINK",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:ANAK ANJINK\nitem1.TEL;waid=6281354079196:6281354079196\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:rimuru418@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Adri158\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Tokyo 🇯🇵\nitem5.X-ABLabel:───────[ SLIMEBOT ]───────\nEND:VCARD"
+      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:ANAK ANJINK\nitem1.TEL;waid=6288270863279:6288270863279\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:witmawati4@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Adri158\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Tokyo 🇯🇵\nitem5.X-ABLabel:───────[ DanzBot ]───────\nEND:VCARD"
   }, {
    "displayName": "ANJINK",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:ANJINK\nitem1.TEL;waid=6281354079196:6281354079196\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:rimuru418@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Adri158\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Tokyo 🇯🇵\nitem5.X-ABLabel:───────[ SLIMEBOT ]───────\nEND:VCARD"
+      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:ANJINK\nitem1.TEL;waid=6288270863279:6288270863279\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:witmawati4@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Adri158\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Tokyo 🇯🇵\nitem5.X-ABLabel:───────[ DanzBot ]───────\nEND:VCARD"
   }, {
    "displayName": "YATIM",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:YATIM\nitem1.TEL;waid=6281354079196:6281354079196\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:rimuru418@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Adri158\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Tokyo 🇯🇵\nitem5.X-ABLabel:───────[ SLIMEBOT ]───────\nEND:VCARD"
+      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:YATIM\nitem1.TEL;waid=6288270863279:6288270863279\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:witmawati4@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Adri158\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Tokyo 🇯🇵\nitem5.X-ABLabel:───────[ DanzBot ]───────\nEND:VCARD"
   }, {
    "displayName": "KONTOL",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:KONTOL\nitem1.TEL;waid=6281354079196:6281354079196\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:rimuru418@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Adri158\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Tokyo 🇯🇵\nitem5.X-ABLabel:───────[ SLIMEBOT ]───────\nEND:VCARD"
+      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:KONTOL\nitem1.TEL;waid=6288270863279:6288270863279\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:witmawati4@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Adri158\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Tokyo 🇯🇵\nitem5.X-ABLabel:───────[ DanzBot ]───────\nEND:VCARD"
   }, {
    "displayName": "ANAK KONTOL",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:ANAK KONTOL\nitem1.TEL;waid=6281354079196:6281354079196\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:rimuru418@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Adri158\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Tokyo 🇯🇵\nitem5.X-ABLabel:───────[ SLIMEBOT ]───────\nEND:VCARD"
+      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:ANAK KONTOL\nitem1.TEL;waid=6288270863279:6288270863279\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:witmawati4@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Adri158\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Tokyo 🇯🇵\nitem5.X-ABLabel:───────[ DanzBot ]───────\nEND:VCARD"
   }, {
-   "displayName": "ADRI",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:ADRI\nitem1.TEL;waid=6281354079196:6281354079196\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:rimuru418@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Adri158\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Tokyo 🇯🇵\nitem5.X-ABLabel:───────[ SLIMEBOT ]───────\nEND:VCARD"
+   "displayName": "Danzz",
+      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:Danzz\nitem1.TEL;waid=6288270863279:6288270863279\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:witmawati4@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Adri158\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Tokyo 🇯🇵\nitem5.X-ABLabel:───────[ DanzBot ]───────\nEND:VCARD"
   }, {
   }, {
     }]
