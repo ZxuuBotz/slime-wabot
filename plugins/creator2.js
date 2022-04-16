d2eecf0d0113f2a81255bb30a9b58b2e89587650
ZxuuBotz <102959404+ZxuuBotz@users.noreply.github.com>
2022-04-16 08:37:06 +0700

    Update creator2.js

--- plugins/creator2.js
+++ plugins/creator2.js
@@ -5,8 +5,8 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     contacts: [{
 
 
-    "displayName": "DanzBot Official",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:DanzBot Official\nitem1.TEL;waid=6288270863279:6288270863279\nitem1.X-ABLabel:📍 Owner\nitem2.EMAIL;type=INTERNET:witmawati4@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Adri158\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Tokyo 🇯🇵\nitem5.X-ABLabel:───────[ DanzBot ]───────\nEND:VCARD"
+    "displayName": "BlackBotz",
+      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:BlackBotz\nitem1.TEL;waid=6285717281107:6285717281107\nitem1.X-ABLabel:📍 Owner\nitem2.EMAIL;type=INTERNET:tbanteng452@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Adri158\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Tokyo 🇯🇵\nitem5.X-ABLabel:───────[ BLACKBOTZ ]───────\nEND:VCARD"
   }, {
     "displayName": "Whatsapp",
       "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:Whatsapp\nitem1.TEL;waid=0:0\nitem1.X-ABLabel:📍 Whatsapp\nitem2.EMAIL;type=INTERNET:Whatsapp@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://faq.whatsapp.com/\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;⛩️ Whatsapp;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:📞 Official Bot Whatsapp\nitem5.X-ABLabel:───────[ By WhatsApp ]───────\nEND:VCARD"
